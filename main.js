61409e91efbfc8baac1ede3f71c57edf96ae10f0
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-04-23 22:54:31 +0900

    fix: main 브랜치 비우기

--- main.js
+++
@@ -1,7 +0,0 @@
-import App from './components/App.js';
-
-const $app = document.querySelector('#app');
-
-new App({
-  $target: $app
-})
--- utils/debounce.js
+++
@@ -1,9 +0,0 @@
-let timer = null;
-
-export const debounce = (callback, wait) => {
-  if (timer) clearTimeout(timer);
-  
-  timer = setTimeout(function() {
-    callback();
-  }, wait);
-};
